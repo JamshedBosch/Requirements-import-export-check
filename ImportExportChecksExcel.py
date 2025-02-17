16dd0f9c839c974dc65ff59b2280f52a16ae2ae2
Malik <malik.iqbal@hotmail.de>
2025-02-17 11:19:55 +0100

    Checkconfiguration class extracted to seperate module

--- ImportExportChecksExcel.py
+++ ImportExportChecksExcel.py
@@ -4,26 +4,7 @@ import shutil
 from ReportGenerator import ReportGenerator
 from ChecksPPE import ProjectCheckerPPE
 from ChecksSSP import  ProjectCheckerSSP
-
-
-class CheckConfiguration:
-    """Holds configuration and constants for checks."""
-    IMPORT_CHECK = 0
-    EXPORT_CHECK = 1
-
-    PROJECT = {
-        "PPE_MLBW": "PPE/MLBW",
-        "SSP": "SSP"
-    }
-
-    REPORT_FOLDER = os.path.join(os.getcwd(), "report")
-
-
-    IMPORT_FOLDERS = {
-        IMPORT_CHECK: r"D:\AUDI\Import_Reqif2Excel_Converted",
-        EXPORT_CHECK: r"D:\AUDI\Export_Reqif2Excel_Converted"
-    }
-
+from projconfig import CheckConfiguration
 
 
 class ChecksProcessorExcel:
